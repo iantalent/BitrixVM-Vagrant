4ed89381c26bb3048a886180fcc1842f09202718
iantalent <36401271+iantalent@users.noreply.github.com>
2025-02-11 16:46:59 +0200

    Update bitrix-install.sh

--- provision/bitrix-install.sh
+++ provision/bitrix-install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# update before installation
-dnf clean all && dnf update -y
-
 # download bitrixVM and execute
-wget http://repo.bitrix.info/dnf/bitrix-env-9.sh && chmod +x bitrix-env-9.sh && ./bitrix-env-9.sh -s -p -H server -M "password"
+wget http://repo.bitrix.info/dnf/bitrix-env-9.sh && chmod +x bitrix-env-9.sh && ./bitrix-env-9.sh -s -p -H server1
