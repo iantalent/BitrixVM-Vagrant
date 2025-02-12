8584f94c1640d2c8aa126b2caa1a2391885896da
iantalent <36401271+iantalent@users.noreply.github.com>
2025-02-12 17:39:35 +0200

    Update before-bitrix-install.sh

--- provision/before-bitrix-install.sh
+++ provision/before-bitrix-install.sh
@@ -7,3 +7,9 @@ dnf clean all && dnf update -y
 sed -i '/\(^SELINUX=\).*/ s//\1disabled/' /etc/sysconfig/selinux
 
 grubby --update-kernel ALL --args selinux=0
+
+# create if not exists
+id -u bitrix &> /dev/null || useradd --no-create-home bitrix
+
+# change /home/bitrix owner
+chown bitrix:bitrix /home/bitrix
