8e6a8fc2ddfa22e9800486fa22e465491c6300ab
iantalent <36401271+iantalent@users.noreply.github.com>
2025-02-28 22:17:24 +0200

    Update bitrix-install.sh

--- provision/bitrix-install.sh
+++ provision/bitrix-install.sh
@@ -5,6 +5,6 @@ echo "Installing BitrixVM"
 # download bitrixVM and execute
 wget http://repo.bitrix.info/dnf/bitrix-env-9.sh && chmod +x bitrix-env-9.sh && ./bitrix-env-9.sh -s -p -H server1
 
-echo "Remoing root user /root/menu.sh autostart"
+echo "Removing root user /root/menu.sh autostart"
 
 sed -i '/\(^~\/menu\.sh\).*/ s//#\1/' /root/.bash_profile
